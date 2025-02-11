b6ee0ea844c81d92800eba47819b2be197ce15d0
Cole J. Calamos <cole@calamos.family>
2025-02-10 21:29:42 -0600

    btop

--- hosts/fw-13-12thgen/configuration.nix
+++ hosts/fw-13-12thgen/configuration.nix
@@ -63,7 +63,6 @@
 
   # Configure keymap in X11
   services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
--- users/ccalamos/home.nix
+++ users/ccalamos/home.nix
@@ -18,6 +18,8 @@
     VISUAL = "nvim";
   };
 
+  nixpkgs.config.allowUnfree = true;
+
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
