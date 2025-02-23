637772ed0c3690432be203a1e1898357a58b2b69
Cole J. Calamos <cole@calamos.family>
2025-02-22 21:19:14 -0600

    Trying to fix hyprland

--- programs/hyprland/default.nix
+++ programs/hyprland/default.nix
@@ -6,12 +6,26 @@
 
       xwayland.enable = true;
 
+      systemd = {
+        enable = true;
+        variables = [ "--all" ]; # fix for https://wiki.hyprland.org/Nix/Hyprland-on-Home-Manager/#programs-dont-work-in-systemd-services-but-do-on-the-terminal
+        # TODO(hyprland): experiment with whether this is required.
+        # Same as default, but stop the graphical session too
+        extraCommands = lib.mkBefore [
+          "systemctl --user stop graphical-session.target"
+          "systemctl --user start hyprland-session.target"
+        ];
+      };
+
       extraConfig = ''
-        exec-once = hyprpaper
       '';
 
       settings = {
         "$mod" = "SUPER";
+
+        exec-once = [
+          ''${inputs.hyprpaper.packages."${pkgs.system}".default}/bin/hyprpaper''
+        ];
 
         input = {
           numlock_by_default = true;
