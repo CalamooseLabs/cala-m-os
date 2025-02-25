c1e3b799aaf21c7f4da436819a59109665cb11d1
Cole J. Calamos <cole@calamos.family>
2025-02-25 10:17:45 -0600

    Added built in traces

--- users/_core/default.nix
+++ users/_core/default.nix
@@ -10,25 +10,32 @@ let
   user_configuration = "${user_config_path}/configuation.nix";
   user_home_configuration = "${user_config_path}/home.nix";
 
-  makeProgramConfigs = name: filename: import (programs_path + "/${name}/${filename}") {
-    inherit username user_home_path;
-  };
+  # Add tracing to log program imports
+  makeProgramConfigs = name: filename: builtins.trace
+    "Importing ${name}/${filename}"
+    (import (programs_path + "/${name}/${filename}") {
+      inherit username user_home_path;
+    });
 
-  config_imports = map (name: makeProgramConfigs name "configuration.nix") import_programs;
-  home_imports = map (name: makeProgramConfigs name "home.nix") import_programs;
+  # Add tracing to log the list of programs being imported
+  config_imports = builtins.trace "Config imports: ${import_programs}"
+    (map (name: makeProgramConfigs name "configuration.nix") import_programs);
+
+  home_imports = builtins.trace "Home imports: ${import_programs}"
+    (map (name: makeProgramConfigs name "home.nix") import_programs);
 in
 {
   imports = [
-    ./configuration.nix { inherit username; } # Core Config
-    user_configuration # User Config
+    (builtins.trace "Importing core configuration" ./configuration.nix { inherit username; }) # Core Config
+    (builtins.trace "Importing user configuration" user_configuration) # User Config
   ] ++ config_imports;
 
   home-manager = {
     users = {
       "${username}" = {
         imports = [
-          ./home.nix { inherit username user_home_path; } # Core Home Config
-          user_home_configuration # User Home Config
+          (builtins.trace "Importing core home configuration" ./home.nix { inherit username user_home_path; }) # Core Home Config
+          (builtins.trace "Importing user home configuration" user_home_configuration) # User Home Config
         ] ++ home_imports;
       };
     };
