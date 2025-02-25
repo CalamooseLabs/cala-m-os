4fd366f88b9ad5b09aa238b5a6e1d180fd139ad8
Cole J. Calamos <cole@calamos.family>
2025-02-25 10:09:34 -0600

    Fixed bugs

--- users/_core/default.nix
+++ users/_core/default.nix
@@ -1,11 +1,14 @@
-{ username, import_programs, user_home ? null ... }:
+{ username, import_programs, user_home ? null, ... }:
 
 let
   user_home_path = if user_home == null then "/home/${username}" else user_home;
 
   root_path = "${builtins.dirname ./.}/../..";
-  user_config_path = "${rootpath}/users/${username}"/home.nix;
-  programs_path = "${rootpath}/programs"/.;
+  user_config_path = "${root_path}/users/${username}/home.nix";
+  programs_path = "${root_path}/programs";
+
+  user_configuration = "${user_config_path}/configuation.nix";
+  user_home_configuration = "${user_config_path}/home.nix";
 
   makeProgramConfigs = name: filename: import "${toString (programs_path + "/${name}/${filename}")}";
 
@@ -15,7 +18,7 @@ in
 {
   imports = [
     ./configuration.nix { inherit username; } # Core Config
-    user_config_path/configuration.nix # User Config
+    user_configuration # User Config
   ] ++ config_imports;
 
   # This is where we will take the inputs and will use the importer from ./programs
@@ -24,6 +27,7 @@ in
       "${username}" = {
         imports = [
           ./home.nix { inherit username user_home_path ; } # Core Home Config
+          user_home_configuration # User Home Config
         ] ++ home_imports;
       };
     };
