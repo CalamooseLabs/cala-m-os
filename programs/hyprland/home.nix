c5f3d25b85a995c7a678d45ff5437c075f500fea
Cole J. Calamos <cole@calamos.family>
2025-03-01 22:07:09 -0600

    Fixes

--- programs/hyprland/home.nix
+++ programs/hyprland/home.nix
@@ -4,7 +4,7 @@
   wayland.windowManager.hyprland = {
       enable = true;
       package = inputs.hyprland.packages."${pkgs.system}".hyprland;
-      portalPackage = inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
+      # portalPackage = inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
 
       xwayland.enable = true;
 
