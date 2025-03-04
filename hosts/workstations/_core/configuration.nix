7b592b9f2206911d22ffee53670ec5beefa35fb2
Cole J. Calamos <cole@calamos.family>
2025-03-04 15:28:38 -0600

    XDG Portal Work

--- hosts/workstations/_core/configuration.nix
+++ hosts/workstations/_core/configuration.nix
@@ -96,6 +96,18 @@ in
     };
   };
 
+  # Extra Portal Configuration
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = [pkgs.xdg-desktop-portal-gtk pkgs.xdg-desktop-portal];
+    configPackages = [
+      pkgs.xdg-desktop-portal-gtk
+      pkgs.xdg-desktop-portal-hyprland
+      pkgs.xdg-desktop-portal
+    ];
+  };
+
   # Original State Version
   system.stateVersion = "24.11"; # Do not change
 }
