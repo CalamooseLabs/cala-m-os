cd6d70ffa04f4c9297cbcc2dfd6764d87b8fc74d
Cole J. Calamos <cole@calamos.family>
2025-03-10 13:35:00 -0500

    Trying to fix portals

--- programs/hyprland/configuration.nix
+++ programs/hyprland/configuration.nix
@@ -8,11 +8,11 @@
   xdg.portal = {
     enable = true;
     wlr.enable = true;
-    extraPortals = [pkgs.xdg-desktop-portal-gtk pkgs.xdg-desktop-portal];
-    configPackages = [
-      pkgs.xdg-desktop-portal-gtk
+    extraPortals = [
       pkgs.xdg-desktop-portal-hyprland
-      pkgs.xdg-desktop-portal
+    ];
+    configPackages = [
+      pkgs.xdg-desktop-portal-hyprland
     ];
 
     config.common.default = "*";
