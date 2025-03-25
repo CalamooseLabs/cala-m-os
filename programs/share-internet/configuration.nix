513f881cc4193bab0a4e5f0228562e71681e7d26
Cole J. Calamos <cole@calamos.family>
2025-03-25 13:27:46 -0500

    Wireguard fix

--- programs/share-internet/configuration.nix
+++ programs/share-internet/configuration.nix
@@ -2,6 +2,12 @@
   # Enable nftables service
   networking.nftables.enable = true;
 
+  networking.firewall.allowedUDPPorts = [
+    53
+    67
+    68
+  ];
+
   # Basic dnsmasq configuration
   services.dnsmasq.enable = true;
 }
--- programs/vpn/configuration.nix
+++ programs/vpn/configuration.nix
@@ -1,14 +1,20 @@
 {...}: {
-  # Wireguard VPN nmtui
-  # TODO: Add more declarative approach for setting vpn connections (Such as SOPS)
-  networking.firewall = {
-    extraCommands = ''
-      ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN
-      ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN
-    '';
-    extraStopCommands = ''
-      ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN || true
-      ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN || true
-    '';
-  };
+  networking.firewall.allowedUDPPorts = [51820];
+
+  # Remove the incompatible iptables commands
+  networking.firewall.extraCommands = "";
+  networking.firewall.extraStopCommands = "";
+
+  # Enable nftables
+  networking.nftables.enable = true;
+
+  networking.nftables.ruleset = ''
+    # WireGuard-specific rules to bypass reverse path filtering
+    table inet filter {
+      chain nixos-fw-rpfilter {
+        udp sport 51820 return
+        udp dport 51820 return
+      }
+    }
+  '';
 }
