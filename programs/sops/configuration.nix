0dee6ab825ba157c2d14de99694f3d38db8c163c
Cole J. Calamos <it@calamos.family>
2025-04-08 00:17:21 -0500

    Updates

--- programs/sops/configuration.nix
+++ programs/sops/configuration.nix
@@ -1,8 +1,2 @@
-{pkgs, ...}: {
-  environment.systemPackages = [
-    # pkgs.sops
-    # pkgs.age
-    # pkgs.age-plugin-yubikey
-    pkgs.age-plugin-fido2-hmac
-  ];
+{...}: {
 }
--- programs/sops/home.nix
+++ programs/sops/home.nix
@@ -1,8 +1,5 @@
 {pkgs, ...}: {
   home.packages = [
     pkgs.sops
-    pkgs.age
-    # pkgs.age-plugin-yubikey
-    # pkgs.age-plugin-fido2-hmac
   ];
 }
