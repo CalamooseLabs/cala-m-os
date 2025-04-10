5f575bded20a363cca40055964b40001ec05fac3
Cole J. Calamos <it@calamos.family>
2025-04-10 16:18:19 -0500

    Moved OBS to home manager

--- programs/obs-studio/configuration.nix
+++ programs/obs-studio/configuration.nix
@@ -1,9 +1,2 @@
-{pkgs, ...}: {
-  programs.obs-studio = {
-    enable = true;
-    plugins = with pkgs.obs-studio-plugins; [
-      wlrobs
-      obs-pipewire-audio-capture
-    ];
-  };
+{...}: {
 }
--- programs/obs-studio/home.nix
+++ programs/obs-studio/home.nix
@@ -1,2 +1,9 @@
-{...}: {
+{pkgs, ...}: {
+  programs.obs-studio = {
+    enable = true;
+    plugins = [
+      pkgs.obs-studio-plugins.wlrobs
+      pkgs.obs-studio-plugins.obs-pipewire-audio-capture
+    ];
+  };
 }
