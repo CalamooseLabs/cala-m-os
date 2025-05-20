23c8298fdb594139da1eae4d400de0434e5abfb4
Cole J. Calamos <it@calamos.family>
2025-05-20 15:27:29 -0500

    Trying to fix fingerprint

--- modules/fingerprint-scanner/configuration.nix
+++ modules/fingerprint-scanner/configuration.nix
@@ -7,7 +7,7 @@
   # Install the driver
   services.fprintd.enable = true;
   # If simply enabling fprintd is not enough, try enabling fprintd.tod...
-  services.fprintd.tod.enable = true;
+  # services.fprintd.tod.enable = true;
   # ...and use one of the next four drivers
-  services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix; # Goodix driver module
+  # services.fprintd.tod.driver 11= pkgs.libfprint-2-tod1-goodix; # Goodix driver module
 }
