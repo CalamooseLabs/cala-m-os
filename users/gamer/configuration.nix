2e1b878663c83fbe28baa86073ac9bd112ff3a91
Cole J. Calamos <it@calamos.family>
2025-09-02 12:26:04 -0600

    Removed unused

--- users/gamer/configuration.nix
+++ users/gamer/configuration.nix
@@ -25,16 +25,6 @@
   programs.gamemode.enable = true; # for performance mode
 
   environment.systemPackages = with pkgs; [
-    mumble # install voice-chat
     protonup-qt # GUI for installing custom Proton versions like GE_Proton
-    (retroarch.override {
-      cores = with libretro; [
-        # decide what emulators you want to include
-        puae # Amiga 500
-        scummvm
-        dosbox
-      ];
-    })
-    teamspeak_client # install voice-chat
   ];
 }
