317f92f3cf1da311cdf1669e6c099157f8a7c230
Cole J. Calamos <it@calamos.family>
2025-09-02 15:05:55 -0600

    Added steam to launch on gamer

--- users/gamer/home.nix
+++ users/gamer/home.nix
@@ -1,3 +1,13 @@
 {...}: {...}: {
   home.packages = [];
+
+  wayland.windowManager.hyprland = {
+    settings = {
+      exec-once = [
+        "steam"
+      ];
+
+      bind = ["$mod, S, exec, steam"];
+    };
+  };
 }
