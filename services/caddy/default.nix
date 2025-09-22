b8657102b541deb961ebdefa1374b3953ef96a69
Cole J. Calamos <it@calamos.family>
2025-09-22 16:20:43 -0600

    come on

--- services/caddy/default.nix
+++ services/caddy/default.nix
@@ -5,7 +5,8 @@
 }: let
   caddyWithCloudflare = pkgs.caddy.withPlugins {
     plugins = ["github.com/caddy-dns/cloudflare@v0.2.1"];
-    hash = "sha256-j+xUy8OAjEo+bdMOkQ1kVqDnEkzKGTBIbMDVL7YDwDY=";
+    # hash = "sha256-j+xUy8OAjEo+bdMOkQ1kVqDnEkzKGTBIbMDVL7YDwDY=";
+    hash = lib.fakeSha256;
   };
 
   serviceDefaults = {
