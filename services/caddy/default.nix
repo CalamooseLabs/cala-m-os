bacc4549a5c233fa0d9bc38db6402ca04d6adaf2
Cole J. Calamos <it@calamos.family>
2025-09-23 09:26:54 -0600

    Updated hash

--- services/caddy/default.nix
+++ services/caddy/default.nix
@@ -31,7 +31,7 @@ in {
     enable = true;
     package = pkgs.caddy.withPlugins {
       plugins = ["github.com/caddy-dns/cloudflare@v0.2.1"];
-      hash = "sha256-2D7dnG50CwtCho+U+iHmSj2w14zllQXPjmTHr6lJZ/A=";
+      hash = "sha256-j+xUy8OAjEo+bdMOkQ1kVqDnEkzKGTBIbMDVL7YDwDY=";
     };
 
     # Global configuration for Cloudflare DNS
