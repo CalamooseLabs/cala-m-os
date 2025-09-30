f21b3256304ca65cac1a47060c029d2590be99a6
Cole J. Calamos <it@calamos.family>
2025-09-30 14:59:03 -0600

    Testing backups

--- modules/git/home.nix
+++ modules/git/home.nix
@@ -4,7 +4,7 @@
     userName = cala-m-os.globalFullName;
     userEmail = cala-m-os.globalDefaultEmail;
     signing = {
-      key = "50D56BF0B93CA212";
+      key = "50D56BF0B93CA212"; # Backup Key: 8AA1F614601153B5
       signByDefault = true;
     };
     extraConfig = {
