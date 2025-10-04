335ecbc87b161615f991d804af45fb2cc146b203
Cole J. Calamos <it@calamos.family>
2025-10-03 19:38:55 -0600

    Trying to blacklist

--- machines/workstations/A520M-ITX/configuration.nix
+++ machines/workstations/A520M-ITX/configuration.nix
@@ -15,14 +15,29 @@
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  boot.kernelParams = [
-    "amd_iommu=on"
-    "iommu=pt"
-  ];
-
+  # Mellanox works out of the box on host
   boot.kernelModules = [
+    # "mlx5_core"  # or mlx4_core depending on your card
+    # "mlx5_ib"    # If using InfiniBand
+    # VFIO for GPU passthrough
     "vfio"
     "vfio_iommu_type1"
     "vfio_pci"
+  ];
+
+  # Only blacklist NVIDIA GPU drivers
+  boot.blacklistedKernelModules = [
+    "nvidia"
+    "nvidia_modeset"
+    "nvidia_uvm"
+    "nvidia_drm"
+    "nouveau"
+  ];
+
+  # Bind only GPU to VFIO
+  boot.kernelParams = [
+    "amd_iommu=on"
+    "iommu=pt"
+    "vfio-pci.ids=10de:2705,10de:22bb" # Only GPU IDs
   ];
 }
