47553551e023c0305b74d8fa8dbb32df6f630311
Cole J. Calamos <it@calamos.family>
2025-10-07 16:04:37 -0600

    Removing htpc for now until testing

--- hosts/lab/vms.nix
+++ hosts/lab/vms.nix
@@ -15,19 +15,19 @@
         }
       ];
     };
-    "htpc" = {
-      devices = ["rtx-5090"];
-      storage = 100; # GBs
-      macID = "02";
-      shares = [
-        #   {
-        #     proto = "virtiofs";
-        #     tag = "games";
-        #     source = "/vm-data";
-        #     mountPoint = "/mnt/games";
-        #   }
-      ];
-    };
+    # "htpc" = {
+    #   devices = ["rtx-5090"];
+    #   storage = 100; # GBs
+    #   macID = "02";
+    #   shares = [
+    #     #   {
+    #     #     proto = "virtiofs";
+    #     #     tag = "games";
+    #     #     source = "/vm-data";
+    #     #     mountPoint = "/mnt/games";
+    #     #   }
+    #   ];
+    # };
     "torrent" = {
       devices = [];
       storage = 100;
@@ -38,14 +38,7 @@
       devices = ["rtx-4060"];
       storage = 100; # GBs
       macID = "04";
-      shares = [
-        # {
-        #   proto = "virtiofs";
-        #   tag = "games";
-        #   source = "/vm-data";
-        #   mountPoint = "/mnt/games";
-        # }
-      ];
+      shares = [];
     };
   };
 
