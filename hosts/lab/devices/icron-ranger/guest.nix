114657a13b3398c03d2f5b855d0815007e62fe68
Cole J. Calamos <it@calamos.family>
2025-10-09 10:50:38 -0600

    trying to add cypress and terminus

--- hosts/lab/devices/icron-ranger/guest.nix
+++ hosts/lab/devices/icron-ranger/guest.nix
@@ -2,11 +2,15 @@
   microvm.devices = [
     {
       bus = "usb";
-      path = "vendorid=0x04b4,productid=0x6506"; # Icron Ranger
+      path = "vendorid=0x04b4,productid=0x6506"; # Icron Ranger Cypress
     }
     {
       bus = "usb";
-      path = "vendorid=0x046d,productid=0xc52b"; # Logitech
+      path = "vendorid=0x1a40,productid=0x0101"; # Icron Ranger Cypress
     }
+    # {
+    #   bus = "usb";
+    #   path = "vendorid=0x046d,productid=0xc52b"; # Logitech
+    # }
   ];
 }
--- hosts/lab/devices/icron-ranger/host.nix
+++ hosts/lab/devices/icron-ranger/host.nix
@@ -2,7 +2,8 @@
   services.udev.extraRules = ''
     # Icron Ranger
     SUBSYSTEM=="usb", ATTR{idVendor}=="04b4", ATTR{idProduct}=="6506", GROUP="kvm"
+    SUBSYSTEM=="usb", ATTR{idVendor}=="1a40", ATTR{idProduct}=="0101", GROUP="kvm"
     # Unifying Logitech
-    SUBSYSTEM=="usb", ATTR{idVendor}=="046d", ATTR{idProduct}=="c52b", GROUP="kvm"
+    #SUBSYSTEM=="usb", ATTR{idVendor}=="046d", ATTR{idProduct}=="c52b", GROUP="kvm"
   '';
 }
