666349417f208139c02be7310d5c2bc2c78e6b73
Cole J. Calamos <it@calamos.family>
2025-10-10 15:16:06 -0600

    Added 8BitDo

--- hosts/lab/devices/icron-ranger/guest.nix
+++ hosts/lab/devices/icron-ranger/guest.nix
@@ -8,5 +8,9 @@
       bus = "usb";
       path = "vendorid=0x046d,productid=0xc52b"; # Logitech
     }
+    {
+      bus = "usb";
+      path = "vendorid=0x2dc8,productid=0x3109"; # 8 Bitdo Pro 3
+    }
   ];
 }
--- hosts/lab/devices/icron-ranger/host.nix
+++ hosts/lab/devices/icron-ranger/host.nix
@@ -3,5 +3,6 @@
     # Icron Ranger
     SUBSYSTEM=="usb", ATTR{idVendor}=="04b4", ATTR{idProduct}=="6506", GROUP="kvm"
     SUBSYSTEM=="usb", ATTR{idVendor}=="046d", ATTR{idProduct}=="c52b", GROUP="kvm"
+    SUBSYSTEM=="usb", ATTR{idVendor}=="2dc8", ATTR{idProduct}=="3109", GROUP="kvm"
   '';
 }
