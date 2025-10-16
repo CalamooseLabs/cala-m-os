b42d7ba094b2cd0f2d10c7a52d2fbb9b6fd5f5b3
Cole J. Calamos <it@calamos.family>
2025-10-16 11:04:46 -0600

    Trying with ACS Overridge

--- hosts/lab/devices/bus-01-02/guest.nix
+++ hosts/lab/devices/bus-01-02/guest.nix
@@ -1,10 +1,10 @@
 {...}: {
   microvm.devices = [
     # Group 6 - all devices by PCI address
-    {
-      bus = "pci";
-      path = "0000:c1:00.0";
-    } # Non-Essential (bound via ID)
+    # {
+    #   bus = "pci";
+    #   path = "0000:c1:00.0";
+    # } # Non-Essential (bound via ID)
     {
       bus = "pci";
       path = "0000:c1:00.4";
