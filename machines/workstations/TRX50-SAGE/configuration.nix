748690fab4a8f050ab409a519860e6c764f89414
Cole J. Calamos <it@calamos.family>
2025-10-16 09:09:08 -0600

    No spaces in kernelParams

--- machines/workstations/TRX50-SAGE/configuration.nix
+++ machines/workstations/TRX50-SAGE/configuration.nix
@@ -26,6 +26,6 @@
   boot.kernelParams = [
     "amd_iommu=on"
     "iommu=pt"
-    "vfio-pci ids=10de:2b85,10de:22e8,10de:2882,10de:22be,1022:14c9"
+    "vfio-pci.ids=10de:2b85,10de:22e8,10de:2882,10de:22be,1022:14c9"
   ];
 }
