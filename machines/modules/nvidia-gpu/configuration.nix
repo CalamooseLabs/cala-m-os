20907fb71bf081872aa1ea51badfe82b9d2ad24f
Cole J. Calamos <it@calamos.family>
2025-10-17 16:18:41 -0600

    Deprecated mesa

--- machines/modules/nvidia-gpu/configuration.nix
+++ machines/modules/nvidia-gpu/configuration.nix
@@ -9,13 +9,13 @@
       enable32Bit = true;
 
       extraPackages = with pkgs; [
-        mesa.drivers
+        mesa
         vulkan-loader
         vulkan-validation-layers
         vulkan-extension-layer
       ];
       extraPackages32 = with pkgs.pkgsi686Linux; [
-        mesa.drivers
+        mesa
         vulkan-loader
         vulkan-validation-layers
       ];
