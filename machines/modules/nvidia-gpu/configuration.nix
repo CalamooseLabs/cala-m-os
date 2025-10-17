a06ec3845d672cae8535bf83b14c8a74697c04fb
Cole J. Calamos <it@calamos.family>
2025-10-17 16:15:53 -0600

    Adding some more

--- machines/modules/nvidia-gpu/configuration.nix
+++ machines/modules/nvidia-gpu/configuration.nix
@@ -7,6 +7,18 @@
     graphics = {
       enable = true;
       enable32Bit = true;
+
+      extraPackages = with pkgs; [
+        mesa.drivers
+        vulkan-loader
+        vulkan-validation-layers
+        vulkan-extension-layer
+      ];
+      extraPackages32 = with pkgs.pkgsi686Linux; [
+        mesa.drivers
+        vulkan-loader
+        vulkan-validation-layers
+      ];
     };
 
     nvidia = {
@@ -18,11 +30,17 @@
     };
   };
 
-  hardware.graphics.extraPackages = with pkgs; [
-    vulkan-loader
-    vulkan-validation-layers
-    vulkan-extension-layer
-  ];
+  xdg.portal.enable = true;
+
+  environment.sessionVariables = {
+    LD_LIBRARY_PATH = "/run/opengl-driver/lib:/run/opengl-driver-32/lib";
+    WLR_NO_HARDWARE_CURSORS = "1";
+    NIXOS_OZONE_WL = "1";
+  };
+
+  hardware = {
+    opengl.enable = true;
+  };
 
   services.xserver.videoDrivers = ["nvidia"];
 
