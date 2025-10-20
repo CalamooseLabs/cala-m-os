ba0f75d623bb064db3965e4dc6eebd0fd40e9248
Cole J. Calamos <it@calamos.family>
2025-10-20 12:47:16 -0600

    trying things

--- modules/git/home.nix
+++ modules/git/home.nix
@@ -1,8 +1,12 @@
 {cala-m-os, ...}: {
   programs.git = {
     enable = true;
-    userName = cala-m-os.globalFullName;
-    userEmail = cala-m-os.globalDefaultEmail;
+    settings = {
+      user = {
+        name = cala-m-os.globalFullName;
+        email = cala-m-os.globalDefaultEmail;
+      };
+    };
     signing = {
       key = "50D56BF0B93CA212"; # Backup Key: 8AA1F614601153B5
       signByDefault = true;
