83c6096f25e56dbe9fe382f8a4d2c76d7b5d5865
Cole J. Calamos <it@calamos.family>
2025-11-04 16:31:13 -0700

    Almost there

--- modules/geforce-now/home.nix
+++ modules/geforce-now/home.nix
@@ -25,18 +25,8 @@
       "GeForceNOW:app/com.nvidia.geforcenow//master"
     ];
 
-    # Wayland-specific overrides for GeForce Now
     overrides = {
-      # global = {
-      # Force Wayland by default
-      # Context.sockets = ["wayland" "!x11" "!fallback-x11"];
-      # };
       "com.nvidia.geforcenow" = {
-        Environment = {
-          MESA_LOADER_DRIVER_OVERRIDE = "";
-          VK_ICD_FILENAMES = "";
-          ANV_DEBUG = "video-decode,video-encode";
-        };
         Context = {
           sockets = [
             "!wayland"
