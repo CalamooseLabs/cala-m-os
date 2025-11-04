9aef5b8c04523ff362f3812eaf42feca0053f8b6
Cole J. Calamos <it@calamos.family>
2025-11-04 11:03:00 -0700

    Updated refresh ratesa

--- machines/workstations/FW13-12XXP/modules/hyprland/home.nix
+++ machines/workstations/FW13-12XXP/modules/hyprland/home.nix
@@ -10,7 +10,7 @@ in {
       monitor = [
         "${internalDisplay}" # Laptop Screen
         "desc:Microstep MSI MP341CQ 0x00000077, 3440x1440@60, 2256x0, 1" # Work Widescreen
-        "desc:Microstep MSI G272QPF 0x01010101, 2560x1440@60, -1432x-1504, 1" # Office Left Monitor
+        "desc:Microstep MSI G272QPF 0x01010101, 2560x1440@120, -1432x-1504, 1" # Office Left Monitor
         "DP-6, 2560x1440@60,  1128x-1504, 1" # Office Right Monitor
       ];
 
