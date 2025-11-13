668b1b05da82401e6c3553a0a8f0e10059ce7e6a
Cole J. Calamos <it@calamos.family>
2025-11-13 10:10:05 -0700

    Updated gamescope scripts

--- users/gamer-vm/home.nix
+++ users/gamer-vm/home.nix
@@ -6,8 +6,12 @@
 
   wayland.windowManager.hyprland = {
     settings = {
+      monitor = [
+        "HDMI-1, 3840x2160@60,  0x0, 1" # 4K TV
+      ];
+
       exec-once = [
-        "gamescope -f --force-grab-cursor --mangoapp --expose-wayland -- steam -tenfoot"
+        "gamescope -f --force-grab-cursor --mangoapp -e -H 2160 -W 3840 --expose-wayland -- steam -tenfoot"
       ];
     };
   };
