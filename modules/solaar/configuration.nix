d9ac38b0f04625570894db5e1a6d18c86fef11e6
Cole J. Calamos <it@calamos.family>
2025-12-19 11:43:29 -0700

    Updated to nixpkgs

--- modules/solaar/configuration.nix
+++ modules/solaar/configuration.nix
@@ -1,9 +1,16 @@
-{inputs, ...}: {
-  imports = [
-    inputs.solaar.nixosModules.default
+{
+  inputs,
+  pkgs,
+  ...
+}: {
+  # imports = [
+  #   inputs.solaar.nixosModules.default
+  # ];
+  environment.systemPackages = [
+    pkgs.solaar
   ];
 
-  services.solaar.enable = true;
+  # services.solaar.enable = true;
 
   hardware.logitech.wireless = {
     enable = true;
