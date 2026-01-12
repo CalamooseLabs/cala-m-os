8985b845a49f28ce3143c7096d049b4f5cc91d8b
Cole J. Calamos <it@calamos.family>
2026-01-12 13:12:11 -0700

    Retying the rerun script to be commented out

--- modules/agenix-boot/configuration.nix
+++ modules/agenix-boot/configuration.nix
@@ -1,18 +1,18 @@
 {...}: {
-  systemd.services.agenix-rerun = {
-    description = "Rerun agenix decryption after boot";
-    after = ["pcscd.service"];
-    requires = ["pcscd.service"];
-    wantedBy = ["multi-user.target"];
+  # systemd.services.agenix-rerun = {
+  #   description = "Rerun agenix decryption after boot";
+  #   after = ["pcscd.service"];
+  #   requires = ["pcscd.service"];
+  #   wantedBy = ["multi-user.target"];
 
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
-    };
+  #   serviceConfig = {
+  #     Type = "oneshot";
+  #     RemainAfterExit = true;
+  #   };
 
-    script = ''
-      # Rerun the activation script
-      /run/current-system/activate
-    '';
-  };
+  #   script = ''
+  #     # Rerun the activation script
+  #     /run/current-system/activate
+  #   '';
+  # };
 }
