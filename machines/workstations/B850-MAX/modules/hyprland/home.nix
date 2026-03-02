450b1225640355aded14a6dd155dfe17e8e5be62
Cole J. Calamos <it@calamos.family>
2026-03-02 08:47:19 -0700

    Updated monitors

--- machines/workstations/B850-MAX/modules/hyprland/home.nix
+++ machines/workstations/B850-MAX/modules/hyprland/home.nix
@@ -2,7 +2,7 @@
   wayland.windowManager.hyprland = {
     settings = {
       monitor = [
-        "DP-1, 3840x2160@240, 0x0, 1, bitdepth, 10, cm, hdr, sdrbrightness, 1.2, sdrsaturation, 1.0"
+        "desc:Microstep MPG322UX OLED 0x01010101, 3840x2160@240, 0x0, 1, bitdepth, 10, cm, hdr, sdrbrightness, 1.2, sdrsaturation, 1.0"
       ];
     };
   };
