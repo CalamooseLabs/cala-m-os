ed20386ca9e454844d1594d5e3a06918298b2ba3
Cole J. Calamos <it@calamos.family>
2026-03-02 09:45:32 -0700

    Added media commands to studio

--- machines/workstations/B850-MAX/modules/hyprland/home.nix
+++ machines/workstations/B850-MAX/modules/hyprland/home.nix
@@ -1,9 +1,27 @@
-{...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   wayland.windowManager.hyprland = {
     settings = {
       monitor = [
         "desc:Microstep MPG322UX OLED 0x01010101, 3840x2160@240, 0x0, 1, bitdepth, 10, cm, hdr, sdrbrightness, 1.2, sdrsaturation, 1.0"
       ];
     };
+
+    binde = let
+      pactl = lib.getExe' pkgs.pulseaudio "pactl";
+    in [
+      # Volume - Output
+      ", XF86AudioRaiseVolume, exec, ${pactl} set-sink-volume @DEFAULT_SINK@ +5%"
+      ", XF86AudioLowerVolume, exec, ${pactl} set-sink-volume @DEFAULT_SINK@ -5%"
+      # Volume - Input
+      ", XF86AudioRaiseVolume, exec, ${pactl} set-source-volume @DEFAULT_SOURCE@ +5%"
+      ", XF86AudioLowerVolume, exec, ${pactl} set-source-volume @DEFAULT_SOURCE@ -5%"
+      # Volume - Mute
+      ", XF86AudioMute, exec, ${pactl} set-sink-mute @DEFAULT_SINK@ toggle"
+      ", XF86AudioMute, exec, ${pactl} set-source-mute @DEFAULT_SOURCE@ toggle"
+    ];
   };
 }
