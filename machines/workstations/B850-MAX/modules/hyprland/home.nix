c21ac73b913dcb8c5be92eddb55d308e0748bc77
Cole J. Calamos <it@calamos.family>
2026-03-05 10:13:20 -0700

    Trying other mirror tecls

--- machines/workstations/B850-MAX/modules/hyprland/home.nix
+++ machines/workstations/B850-MAX/modules/hyprland/home.nix
@@ -7,7 +7,9 @@
     settings = {
       monitor = [
         "desc:Microstep MPG322UX OLED 0x01010101, 3840x2160@240, 0x0, 1, bitdepth, 10, cm, hdr, sdrbrightness, 1.2, sdrsaturation, 1.0"
-        "HDMI-A-1, 3840x2160@60, auto, 1, mirror, DP-3" # Mirror if any other output detected
+        # "HDMI-A-1, 3840x2160@60, auto, 1, mirror, desc:Microstep MPG322UX OLED 0x01010101" # Mirror if any other output detected
+        # "HDMI-A-2, 3840x2160@60, auto, 1, mirror, desc:Microstep MPG322UX OLED 0x01010101" # Mirror if any other output detected
+        ", 3840x2160@60, auto, 1, mirror, desc:Microstep MPG322UX OLED 0x01010101" # Mirror if any other output detected
       ];
 
       binde = let
