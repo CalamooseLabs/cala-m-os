69b98990e8fa6ae6f2cc6980e3303bfac2ddc2e4
Cole J. Calamos <it@calamos.family>
2026-03-10 13:12:47 -0600

    Trying to get elgato prompter to recognize

--- machines/modules/nvidia-gpu/configuration.nix
+++ machines/modules/nvidia-gpu/configuration.nix
@@ -14,7 +14,8 @@
     };
   };
 
-  services.xserver.videoDrivers = ["nvidia"];
+  services.xserver.videoDrivers = ["displaylink" "modesetting" "nvidia"];
+  # services.xserver.videoDrivers = ["nvidia"];
 
   boot.kernelModules = ["nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm"];
 
--- modules/teleprompter/configuration.nix
+++ modules/teleprompter/configuration.nix
@@ -1,14 +1,18 @@
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
+  environment.systemPackages = with pkgs; [
+    displaylink
+  ];
+
+  # evdi kernel module (required for DisplayLink on Wayland)
   boot = {
     extraModulePackages = [config.boot.kernelPackages.evdi];
-    initrd = {
-      kernelModules = [
-        "evdi"
-      ];
-    };
+    initrd.kernelModules = ["evdi"];
   };
 
-  services.xserver.videoDrivers = ["displaylink" "modesetting"];
-
+  # DisplayLink Manager service
   systemd.services.dlm.wantedBy = ["multi-user.target"];
 }
