c5341ec28d9ad1d2cfd2c57a7ab4d66e838d9530
Cole J. Calamos <it@calamos.family>
2026-03-11 15:58:07 -0600

    Updated ashell

--- modules/ashell/home.nix
+++ modules/ashell/home.nix
@@ -2,8 +2,22 @@
   programs.ashell = {
     enable = true;
     settings = {
+      app_launcher_cmd = "rofi";
       appearance = {
         scale_factor = 1.5;
+      };
+      clock = {
+        format = "%a %d %b %r";
+      };
+      modules = {
+        left = ["Workspaces"];
+        center = ["WindowTitle"];
+        right = ["SystemInfo" "Clock" "Settings"]; # Settings includes battery
+      };
+
+      # Configure battery display format
+      settings = {
+        battery_format = "IconAndPercentage"; # Shows both icon and percentage
       };
     };
   };
