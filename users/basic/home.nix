f5ec488da816399e50bdc116338919870cc91528
Cole J. Calamos <it@calamos.family>
2026-03-11 13:11:52 -0600

    Added the exec once for startup apps for simple

--- users/basic/home.nix
+++ users/basic/home.nix
@@ -2,4 +2,19 @@
   services.easyeffects = {
     preset = "FW13";
   };
+
+  wayland.windowManager.hyprland = {
+    settings = {
+      # decoration = {
+      #   shadow_offset = "0 5";
+      #   "col.shadow" = "rgba(00000099)";
+      # };
+
+      # Startup Apps
+      exec-once = [
+        "ashell"
+        "nwg-dock-hyprland"
+      ];
+    };
+  };
 }
