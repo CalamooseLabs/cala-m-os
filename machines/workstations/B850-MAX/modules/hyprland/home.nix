3e77740c7b482e17670330d72fbfbc6b87b5db99
Cole J. Calamos <it@calamos.family>
2026-05-01 10:25:33 -0600

    Up the hz

--- machines/workstations/B850-MAX/modules/hyprland/home.nix
+++ machines/workstations/B850-MAX/modules/hyprland/home.nix
@@ -7,7 +7,7 @@
     settings = {
       monitor = [
         "desc:Microstep MPG322UX OLED 0x01010101, 3840x2160@240, 0x0, 1, bitdepth, 10, cm, hdr, sdrbrightness, 1.2, sdrsaturation, 1.0"
-        ", 3840x2160@60, auto, 1, bitdepth, 10, cm, hdr, sdrbrightness, 1.2, sdrsaturation, 1.0"
+        ", 3840x2160@144, auto, 1, bitdepth, 10, cm, hdr, sdrbrightness, 1.2, sdrsaturation, 1.0"
       ];
 
       binde = let
