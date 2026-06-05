15785dc52b6aba0eaec6213bb7c583c6de0bc951
Cole J. Calamos <it@calamos.family>
2026-06-05 13:34:24 -0600

    Hardware update

--- hosts/devbox/configuration.nix
+++ hosts/devbox/configuration.nix
@@ -21,9 +21,7 @@ in {
       users_list = import_users;
       machine_type = machine_type;
       machine_uuid = machine_uuid;
-      extra_user_modules = {
-        "debugger" = ["yt-dlp"];
-      };
+      extra_user_modules = {};
     })
   ];
 
