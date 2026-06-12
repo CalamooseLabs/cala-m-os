77b90c951adf44cdae0f9afb9a690955e8cdac0f
Cole J. Calamos <it@calamos.family>
2026-06-12 14:39:45 -0600

    Updated hostnames

--- hosts/livedata/vms.nix
+++ hosts/livedata/vms.nix
@@ -4,6 +4,7 @@
       devices = [];
       storage = 600; # GBs
       shares = [];
+      hostOverride = "openreturn";
       ipOverride = "10.1.10.41";
       gatewayOverride = "10.1.10.1";
     };
@@ -11,6 +12,7 @@
       devices = [];
       storage = 100;
       shares = [];
+      hostOverride = "quorumcall";
       ipOverride = "10.1.10.42";
       gatewayOverride = "10.1.10.1";
     };
